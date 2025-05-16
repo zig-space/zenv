edbce4451c86792c0d6cea3298616845cda2b0cf
LeviathanST <phamquochung170505@gmail.com>
2025-05-16 22:48:45 +0700

    add(Reader): ReaderOptions, readStruct

--- src/Reader.zig
+++ src/Reader.zig
@@ -6,6 +6,7 @@ const std = @import("std");
 const Self = @This();
 
 allocator: std.mem.Allocator,
+arena: *std.heap.ArenaAllocator,
 /// Specified that the reader will read
 /// from the `terminal` or a specified `file`.
 ///
@@ -14,20 +15,24 @@ allocator: std.mem.Allocator,
 ///
 /// **Default** is `From.FILE`.
 from: From = .TERM,
-/// The path file contains env (.e.g .env).
-/// Is null if `Zenv.from` is `EnvFrom.TERM`.
-///
-///
-/// Must be specified if `Zenv.from` is `EnvFrom.FILE`
-/// .e.g ("path-to-your-project/.env")
-///
-/// **Default** is `null`.
-env_file_path: ?[]const u8 = null,
-/// Remove whitespace at the beginning and end of the value.
-/// Set `false` if disable.
-///
-/// **Default** is `true`
-trim: bool = true,
+opts: ReaderOptions,
+
+pub const ReaderOptions = struct {
+    /// The path file contains env (.e.g .env).
+    /// Is null if `Zenv.from` is `EnvFrom.TERM`.
+    ///
+    ///
+    /// Must be specified if `Zenv.from` is `EnvFrom.FILE`
+    /// .e.g ("path-to-your-project/.env")
+    ///
+    /// **Default** is `null`.
+    env_file_path: ?[]const u8 = null,
+    /// Remove whitespace at the beginning and end of the value.
+    /// Set `false` if disable.
+    ///
+    /// **Default** is `true`
+    trim: bool = true,
+};
 
 const ReaderError = error{ EmptyFilePath, NotSupportedType };
 
@@ -37,8 +42,47 @@ const From = enum {
     // FILE,
 };
 
+pub fn init(allocator: std.mem.Allocator, from: From, opts: ReaderOptions) !Self {
+    const arena = try allocator.create(std.heap.ArenaAllocator);
+    errdefer allocator.destroy(arena);
+    arena.* = std.heap.ArenaAllocator.init(allocator);
+    return .{
+        .allocator = allocator,
+        .arena = arena,
+        .from = from,
+        .opts = opts,
+    };
+}
+pub fn deinit(self: Self) void {
+    self.arena.deinit();
+    self.allocator.destroy(self.arena);
+}
+
+/// The caller will own the memory of struct,
+/// need to use `destroy()` after finish.
+pub fn readStruct(self: Self, comptime T: type) !*const T {
+    if (@typeInfo(T) != .@"struct") {
+        @compileError(std.fmt.comptimePrint("Expected a `struct` found {s}\n", @typeName(@typeInfo(T))));
+    }
+    const allocator = self.arena.allocator();
+
+    const @"struct" = try allocator.create(T);
+    errdefer allocator.destroy(@"struct");
+
+    inline for (@typeInfo(T).@"struct".fields) |field| {
+        var buffer: [field.name.len]u8 = undefined;
+        _ = std.ascii.upperString(buffer[0..], field.name);
+        const value = try self.readKey(field.type, buffer[0..]) orelse {
+            std.log.warn("Not found environment: {s}", .{buffer});
+            return std.process.GetEnvVarOwnedError.EnvironmentVariableNotFound;
+        };
+
+        @field(@"struct", field.name) = value;
+    }
+    return @"struct";
+}
 /// Return a value with `T` type.
-pub fn readKey(self: Self, comptime T: type, comptime key: []const u8) !T {
+pub fn readKey(self: Self, comptime T: type, key: []const u8) !?T {
     return switch (self.from) {
         .TERM => try self.readKeyFromTerm(T, key[0..]),
     };
@@ -53,27 +97,31 @@ pub fn readKey(self: Self, comptime T: type, comptime key: []const u8) !T {
 //     }
 // }
 
-pub fn readKeyFromTerm(self: Self, comptime T: type, comptime key: []const u8) !T {
+// The caller will own the memory
+pub fn readKeyFromTerm(self: Self, comptime T: type, key: []const u8) !?T {
     std.debug.assert(self.from == .TERM);
-    const env = try std.process.getEnvVarOwned(self.allocator, key[0..]);
-    errdefer self.allocator.free(env);
+    const env = std.process.getEnvVarOwned(self.arena.allocator(), key[0..]) catch |err| switch (err) {
+        std.process.GetEnvVarOwnedError.EnvironmentVariableNotFound => return null,
+        else => return err,
+    };
+    defer self.arena.allocator().free(env);
     return try self.parse(T, env);
 }
 
 pub fn parse(self: Self, comptime T: type, value: []const u8) !T {
     return switch (@typeInfo(T)) {
         .int, .comptime_int => {
-            const parsed = try std.fmt.parseInt(T, value, 10);
-            self.allocator.free(value);
-            return parsed;
+            return try std.fmt.parseInt(T, value, 10);
         },
         .pointer => |pointerInfo| {
             if (pointerInfo.child == u8) {
                 var trimmed = value[0..];
-                if (self.trim) {
+                if (self.opts.trim) {
                     trimmed = std.mem.trim(u8, value[0..], " ");
                 }
-                return trimmed[0..];
+                const new_buffer = try self.arena.allocator().alloc(u8, trimmed.len);
+                @memcpy(new_buffer[0..], trimmed[0..]);
+                return new_buffer[0..];
             } else {
                 std.log.warn("Not supported type: {s}", .{@typeName(T)});
                 return ReaderError.NotSupportedType;
@@ -90,16 +138,31 @@ const expect = std.testing.expect;
 const expectError = std.testing.expectError;
 test "read from terminal" {
     const allocator = std.testing.allocator;
-    const env_reader: Self = .{
-        .from = .TERM,
-        .allocator = allocator,
-    };
+    const env_reader: Self = try .init(allocator, .TERM, .{});
+    defer env_reader.deinit();
+
     const slice = try env_reader.readKey([]const u8, "TEST_SLICE"); // string
-    defer allocator.free(slice);
-    try expect(std.mem.eql(u8, slice[0..], "test"));
-    try expect(@TypeOf(slice) == []const u8);
+    try expect(std.mem.eql(u8, slice.?[0..], "test"));
+    try expect(@TypeOf(slice.?) == []const u8);
 
     const int = try env_reader.readKey(u8, "TEST_NUMBER");
-    try expect(int == 0);
-    try expect(@TypeOf(int) == u8);
+    try expect(int.? == 0);
+    try expect(@TypeOf(int.?) == u8);
+}
+
+test "read struct from terminal" {
+    const allocator = std.testing.allocator;
+    const env_reader: Self = try .init(allocator, .TERM, .{});
+    defer env_reader.deinit();
+
+    const Test = struct {
+        value1: []const u8,
+        value2: u8,
+        value3: u16,
+    };
+
+    const struct_value = try env_reader.readStruct(Test);
+    try expect(std.mem.eql(u8, struct_value.value1, "value1"));
+    try expect(struct_value.value2 == 2);
+    try expect(struct_value.value3 == 3);
 }
